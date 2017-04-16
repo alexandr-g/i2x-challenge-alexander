597676b4b4f3f21b9b52e991d3fb6d7db450c69f
Alexandr <alexgrischuk@gmail.com>
2017-04-16 15:42:00 +0200

    Create Dashboard component and route

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="tc f6 fw6 ttu tracked">Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import App from './components/App';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="login" component={Login} />
     <Route path="home" component={Home} />
+    <Route path="dashboard" component={Dashboard} />
     <Route path="*" component={NotFound} />
   </Route>
 );
