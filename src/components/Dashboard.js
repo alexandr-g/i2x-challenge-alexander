ac1a1b4f4530714e6b6a792f5f2d4192008e3e91
Alexandr <alexgrischuk@gmail.com>
2017-04-16 16:10:44 +0200

    Grab data from remote and put into state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Dashboard = () => {
-  return (
-    <div>
-      <h1 className="tc f6 fw6 ttu tracked">Dashboard</h1>
-    </div>
-  );
-};
+class Dashboard extends Component {
+  state = { results: [] };
+
+  componentWillMount() {
+    axios.get('https://i2x-challenge.herokuapp.com/ai/recording/list/')
+      .then(response => this.setState({ results: response.data.results })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="tc f6 fw6 ttu tracked">Dashboard</h1>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
