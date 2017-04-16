e3be69f114cf4eb18f37aae3f3f41042a28762aa
Alexandr <alexgrischuk@gmail.com>
2017-04-16 20:38:47 +0200

    Convert duration to minutes

--- src/components/RecordDetail.js
+++ src/components/RecordDetail.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StarRatingComponent from 'react-star-rating-component';
+import moment from 'moment';
+require ('moment-duration-format');
 
-const RecordDetail = ({ record }) => {
-  const { created, duration, final_script, rating, url } = record;
+class RecordDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
+  render() {
+    const { created, duration, final_script, rating, url } = this.props.record;
+
+    return (
       <article className="center mw5 mw6-ns hidden ba mv4">
         <h1 className="f4 bg-near-black white mv0 pv2 ph3">
           <StarRatingComponent
@@ -19,11 +26,12 @@ const RecordDetail = ({ record }) => {
             {final_script}
           </p>
         </div>
-        <div>Duration: {duration}</div>
-        <div>URL: {url}</div>
-        <div>CREATED: {created}</div>
+        <div className="ph1"><b>Duration:</b> {moment.duration(duration, 'seconds').format('h [hrs], m [minutes]')}</div>
+        <div className="ph1">URL: {url}</div>
+        <div className="ph1">CREATED: {created}</div>
       </article>
-  );
-};
+    );
+  }
+}
 
 export default RecordDetail;
